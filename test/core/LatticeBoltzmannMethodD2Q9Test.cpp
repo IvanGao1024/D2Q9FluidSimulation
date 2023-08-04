044b7f60737cc7c7b8027fbec1881c06e2ba5ab4
YuanGao <yuangao_ivan@outlook.com>
2023-08-03 18:26:17 -0700

    cartesian matrix add operator = + *

---
+++ test/core/LatticeBoltzmannMethodD2Q9Test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "../../src/core/LatticeBoltzmannMethodD2Q9.h"
+#include "../../src/core/LatticeBoltzmannMethodD2Q9.cpp"
+
+class LatticeBoltzmannMethodD2Q9Test : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+
+    }
+};
+
+TEST_F(LatticeBoltzmannMethodD2Q9Test, Diffusion) {
+        LatticeBoltzmannMethodD2Q9 lbm (20, 20, 
+        LatticeBoltzmannMethodD2Q9::Boundary(LatticeBoltzmannMethodD2Q9::BoundaryType::CONSTANT, 0),
+        LatticeBoltzmannMethodD2Q9::Boundary(LatticeBoltzmannMethodD2Q9::BoundaryType::ADIABATIC),
+        LatticeBoltzmannMethodD2Q9::Boundary(LatticeBoltzmannMethodD2Q9::BoundaryType::CONSTANT, 1),
+        LatticeBoltzmannMethodD2Q9::Boundary(LatticeBoltzmannMethodD2Q9::BoundaryType::CONSTANT, 0),
+        nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+        lbm.step();
+        lbm.mResultingDensityMatrix.print();
+        lbm.mResultingTemperatureMatrix.print();
+}
